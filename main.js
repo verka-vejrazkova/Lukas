a8aa5afb71f20bb5a3ec8fc5b07900180bbf2b62
verka-vejrazkova <71260911+verka-vejrazkova@users.noreply.github.com>
2022-01-05 11:49:08 +0100

    menu

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const hlavicka = document.querySelector("#hlavicka");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+  hlavicka.style.paddingBottom='10rem';
 })
 
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
