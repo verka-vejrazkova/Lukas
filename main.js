b71489e493016c285d8e4e68d35aa5044dc9efdd
verka-vejrazkova <71260911+verka-vejrazkova@users.noreply.github.com>
2022-01-06 15:30:28 +0100

    ceny

--- main.js
+++ main.js
@@ -11,4 +11,9 @@ hamburger.addEventListener("click", () => {
 document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
-}))
+  $("#hlavicka").css("paddingBottom", "");
+ 
+})
+
+
+)
